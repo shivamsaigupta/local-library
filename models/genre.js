77235d3fdcfa4445e60156afdcee8a615732f621
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-17 14:56:36 +0530

    Added export to genre model

--- models/genre.js
+++ models/genre.js
@@ -10,3 +10,6 @@ var GenreSchema = new Schema({
 GenreSchema.virtual("url").get(function() {
   return "/catalog/genre/" + this._id;
 });
+
+// Export the model
+module.exports = mongoose.model("Genre", GenreSchema);
