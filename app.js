82a7afabc360e4794653ce810372a704f7225008
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-17 16:24:43 +0530

    Dummy routing finished

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
