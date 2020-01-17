f33cb63392d372ec1d98518d2dbd9698d58e8688
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-17 17:20:57 +0530

    Created index page

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-// Require controller modules
+// Require controller modules (callback functions)
 var book_controller = require("../controllers/bookController");
 var author_controller = require("../controllers/authorController");
 var genre_controller = require("../controllers/genreController");
