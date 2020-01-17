60e64ef8830835cdfbb730f75cd49f8c3b18039c
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-17 14:51:13 +0530

    Created mongoose models

---
+++ models/author.js
@@ -0,0 +1,43 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, max: 100 },
+  family_name: { type: String, required: true, max: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
+});
+
+// Virtual for author's full name
+
+AuthorSchema.virtual("name").get(function() {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  var fullName = "";
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ", " + this.family_name;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullName = "";
+  }
+
+  return fullName;
+});
+
+// Virtual for author's lifespan
+
+AuthorSchema.virtual("lifespan").get(function() {
+  return (
+    this.date_of_death.getYear() - this.date_of_birth.getYear()
+  ).toString();
+});
+
+// Virtual for author's URL
+
+AuthorSchema.virtual("url").get(function() {
+  return "/catalog/author/" + this._id;
+});
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
---
+++ models/genre.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 100 }
+});
+
+// Virtual for Genre's URL
+GenreSchema.virtual("url").get(function() {
+  return "/catalog/genre/" + this._id;
+});
