440a813e21945b3414e198d14f53eea7e610924b
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-18 10:24:09 +0530

    Implemented functions in controllers

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var moment = require("moment");
 
 var Schema = mongoose.Schema;
 
@@ -17,6 +18,11 @@ var BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function() {
   return "/catalog/bookinstance/" + this._id;
+});
+
+// Virtual for formatted date
+BookInstanceSchema.virtual("due_back_formatted").get(function() {
+  return moment(this.due_back).format("MMMM Do, YYYY");
 });
 
 // Export model
